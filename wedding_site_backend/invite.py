8ad459d3f0815c234171a177ef9ce40ef52208af
Collin Reynolds <creynold@creynold-macbookpro2.roam.corp.google.com>
2018-04-07 22:40:37 -0600

    Add put functionality

--- wedding_site_backend/invite.py
+++ wedding_site_backend/invite.py
@@ -3,6 +3,11 @@ import json
 from sqlalchemy.orm.session import sessionmaker
 from .database import Invite, get_engine
 
+def start_session():
+    engine = get_engine()
+    Session = sessionmaker(engine)
+    return Session()
+
 class InviteResource(object):
 
     def on_get(self, request, response):
@@ -12,14 +17,42 @@ class InviteResource(object):
             response.status = falcon.HTTP_400
             return
 
-        engine = get_engine()
-        Session = sessionmaker(engine)
-        session = Session()
+        session = start_session()
 
         invite = session.query(Invite).get(pass_code)
         if invite is None:
             response.status = falcon.HTTP_404
             return
+
+        response_json = {
+            'first_name': invite.first_name,
+            'last_name': invite.last_name,
+            'email': invite.email,
+            'num_attending': invite.num_attending,
+            'num_song_requests': len(invite.song_requests)
+        }
+
+        response.body = json.dumps(response_json)
+        response.content_type = falcon.MEDIA_JSON
+
+    def on_put(self, request, response):
+        request_json = request.media
+        pass_code = request_json.get('pass_code')
+        if pass_code is None:
+            response.status = falcon.HTTP_400
+            return
+
+        session = start_session()
+
+        invite = session.query(Invite).get(pass_code)
+        if invite is None:
+            response.status = falcon.HTTP_404
+            return
+
+        if 'first_name' in request_json:
+            invite.first_name = request_json['first_name']
+
+        session.commit()
 
         response_json = {
             'first_name': invite.first_name,
