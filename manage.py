ead1a7e367d012cf72090696b438ddbb7cc191fa
Collin Reynolds <creynold@creynold-macbookpro2.roam.corp.google.com>
2018-04-10 23:02:49 -0600

    add add_invite to manage.py

--- manage.py
+++ manage.py
@@ -1,14 +1,30 @@
-from wedding_site_backend.database import get_engine, base
+from wedding_site_backend.database import get_engine, base, Invite, start_session
 from wedding_site_backend.config import Config
 
-if __name__ == '__main__':
-  import argparse
+def get_config():
+    return Config()
 
-  parser = argparse.ArgumentParser()
-  parser.add_argument('operation', help='operation to perform on database',
-                      choices=['create'])
-  args = parser.parse_args()
-
-  config = Config()
-  if args.operation == 'create':
+def create_table(config):
     base.metadata.create_all(get_engine(config))
+
+def add_invite(passcode, session):
+    new_invite = Invite(pass_code=passcode)
+    session.add(new_invite)
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('operation', help='operation to perform on database',
+                        choices=['create', 'add_invite'])
+    parser.add_argument('passcodes', help='Pass codes for the invites', nargs='*')
+    args = parser.parse_args()
+
+    config = get_config()
+    if args.operation == 'create':
+        create_table(config)
+    elif args.operation == 'add_invite':
+        session = start_session(config)
+        for passcode in args.passcodes:
+            add_invite(passcode, session)
+        session.commit()
